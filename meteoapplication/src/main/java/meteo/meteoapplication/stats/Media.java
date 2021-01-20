11d7b06a68785823aac34967cfb3ebb475147295
umbertomaraglino <74837250+umbertomaraglino@users.noreply.github.com>
2021-01-20 16:44:14 +0100

    aggiunta javadoc

--- meteoapplication/src/main/java/meteo/meteoapplication/stats/Media.java
+++ meteoapplication/src/main/java/meteo/meteoapplication/stats/Media.java
@@ -1,10 +1,51 @@
 package meteo.meteoapplication.stats;
 
+/**
+ * 
+ * @author Gioele Pasquini
+ * @author Umberto Maraglino
+ * 
+ * classe che estende l'interfaccia Statistiche 
+ *
+ */
 public class Media implements Statistiche {
     
-	   double mediatemp,mediatemp_max,mediatemp_min,mediafeels_like;
-	    String nome;
+	/**
+	 * 
+	 */
+	/**
+	 * media temperature reali
+	 */
+	private double mediatemp;
+	
+	/**
+	 * media temperature massime
+	 */
+	private double mediatemp_max;
+	
+	/**
+	 * media temperature minime
+	 */
+	private double mediatemp_min;
+	
+	/**
+	 * media temperature percepite
+	 */
+	private double mediafeels_like;
+	
+	/**
+	 * nome città
+	 */
+	 private String nome;
 	    
+	 /**
+	  * Costruttore 
+	  * @param nome
+	  * @param mediatemp
+	  * @param mediatemp_max
+	  * @param mediatemp_min
+	  * @param mediafeels_like
+	  */
     public Media(String nome,double mediatemp,double mediatemp_max,double mediatemp_min,double mediafeels_like) {
         this.nome=nome;
         this.mediafeels_like=mediafeels_like;
@@ -12,57 +53,104 @@ public class Media implements Statistiche {
         this.mediatemp_max=mediatemp_max;
         this.mediatemp_min=mediatemp_min;
     }
-
- 
+    
+    /**
+     * Costruttore vuoto
+     */
     public Media() {
         
     }
     
+    /**
+     * Override metodo stampastats dell'interfaccia
+     * Prende in ingresso una Media_Varianza e restituisce un oggetto Media 
+     */
     @Override
     public Object stampastats(Media_Varianza st) {
         Media m=new Media(st.getNome(),st.getMediatemp(),st.getMediatemp_max(),st.getMediatemp_min(),st.getMediafeels_like());
         return m;
     }
 
+    /**
+	 * Getter mediatemp
+	 * @return mediatemp
+	 */
+	public double getMediatemp() {
+		return mediatemp;
+	}
+	
+	/**
+	 * Setter mediatemp
+	 * @param mediatemp
+	 */
+	public void setMediatemp(double mediatemp) {
+		this.mediatemp = mediatemp;
+	}
+	
+	/**
+	 * Getter media_tempmax
+	 * @return media_tempmax
+	 */
+	public double getMediatemp_max() {
+		return mediatemp_max;
+	}
+	
+	/**
+	 * Setter media_tempmax
+	 * @param mediatemp_max
+	 */
+	public void setMediatemp_max(double mediatemp_max) {
+		this.mediatemp_max = mediatemp_max;
+	}
+	
+	/**
+	 * Getter mediatemp_min
+	 * @return mediatemp_min
+	 */
+	public double getMediatemp_min() {
+		return mediatemp_min;
+	}
+	
+	/**
+	 * Setter mediatemp_min
+	 * @param mediatemp_min
+	 */
+	public void setMediatemp_min(double mediatemp_min) {
+		this.mediatemp_min = mediatemp_min;
+	}
+	
+	/**
+	 * Getter mediafeels_like
+	 * @return mediafeels_like
+	 */
+	public double getMediafeels_like() {
+		return mediafeels_like;
+	}
+	
+	/**
+	 * Setter mediafeels_like
+	 * @param mediafeels_like
+	 */
+	public void setMediafeels_like(double mediafeels_like) {
+		this.mediafeels_like = mediafeels_like;
+	}
     
-    public double getMediatemp() {
-        return mediatemp;
-    }
-    
-    public void setMediatemp(double mediatemp) {
-        this.mediatemp = mediatemp;
-    }
-    
-    public double getMediatemp_max() {
-        return mediatemp_max;
-    }
-    
-    public void setMediatemp_max(double mediatemp_max) {
-        this.mediatemp_max = mediatemp_max;
-    }
-    
-    public double getMediatemp_min() {
-        return mediatemp_min;
-    }
-    
-    public void setMediatemp_min(double mediatemp_min) {
-        this.mediatemp_min = mediatemp_min;
-    }
-    
-    public double getMediafeels_like() {
-        return mediafeels_like;
-    }
-    
-    public void setMediafeels_like(double mediafeels_like) {
-        this.mediafeels_like = mediafeels_like;
-    }
-    
-    public String getNome() {
+	/**
+	 * Getter nome
+	 * @return nome
+	 */
+	public String getNome() {
 		return nome;
 	}
-    
-    public void setNome(String nome) {
+	
+	/**
+	 * Setter nome
+	 * @param nome
+	 */
+	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+
 
 }
