623857010fc552c44637a38a8c400e3665723dd9
umbertomaraglino <74837250+umbertomaraglino@users.noreply.github.com>
2021-01-20 16:32:58 +0100

    aggiunto javadoc

--- meteoapplication/src/main/java/meteo/meteoapplication/exceptions/TypeException.java
+++ meteoapplication/src/main/java/meteo/meteoapplication/exceptions/TypeException.java
@@ -1,14 +1,32 @@
 package meteo.meteoapplication.exceptions;
 
-public class TypeException extends Exception {
-	private static final long serialVersionUID = 1L;
+ 
 
-	public TypeException () {
-		super();
-	}
-	
-	public TypeException (String msg) {
-		super(msg);
-	}
+/**
+ * 
+ * @author Gioele Pasquini
+ * @author Umberto Maraglino
+ * 
+ * Classe che gestisce le eccezioni riguardanti il tipo (di statistiche o di temperatura)
+ *
+ */
+
+ 
+
+public class TypeException extends Exception {
+    
+    private static final long serialVersionUID = 1L;
+
+ 
+
+    public TypeException () {
+        super();
+    }
+    
+    public TypeException (String msg) {
+        super(msg);
+    }
+
+ 
 
 }
