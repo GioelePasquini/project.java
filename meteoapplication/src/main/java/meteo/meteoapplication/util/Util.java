1851af4592c30985e714d05de3605abe40dce742
GioelePasquini <74837241+GioelePasquini@users.noreply.github.com>
2021-01-20 16:46:35 +0100

    aggiunta javadoc

--- meteoapplication/src/main/java/meteo/meteoapplication/util/Util.java
+++ meteoapplication/src/main/java/meteo/meteoapplication/util/Util.java
@@ -1,6 +1,5 @@
 package meteo.meteoapplication.util;
 
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
@@ -11,21 +10,40 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import meteo.meteoapplication.exceptions.CityException;
-
 import meteo.meteoapplication.model.Temperatura;
 import meteo.meteoapplication.service.CurrentService;
 import meteo.meteoapplication.stats.Media_Varianza;
 
- 
+/**
+ * 
+ * @author Gioele Pasquini
+ * @author Umberto Maraglino
+ * 
+ * classe che contiene metodi utili al programma
+ *
+ */
 
 public class Util {
+	
+	/**
+	 * elementi condivisi dai metodi della classe
+	 */
     CurrentService s= new CurrentService ();
     Vector<JSONObject> obj=new Vector<JSONObject>();
     private int counter=1;
-    Vector<Temperatura> v= new Vector<Temperatura>();
+   //FORSE CANC Vector<Temperatura> v= new Vector<Temperatura>();
     
+    /**
+     * metodo che prende in ingresso il nome di una città e il numero di iterazioni e salva le informazioni lette dall'api ogni ora per n volte
+     * e salva le informazioni
+     * 
+     * @param City
+     * @param num_iter
+     * 
+     * @return Vector<Temperatura> contenente tutte le temperature lette 
+     */
     public  Vector<Temperatura> usetimer (String City, int num_iter) {
-        
+    Vector<Temperatura> v= new Vector<Temperatura>();
     Timer timer = new Timer();
     timer.scheduleAtFixedRate(new TimerTask() {
         public void run() {
@@ -42,11 +60,21 @@ public class Util {
         }
             else timer.cancel();
             }
-        },0, 1000);
+        },0, 3600000);
     
         return v;
      }
     
+   /**
+    * metodo che prende in ingresso il JSONObject letto dall'api contenente il giorno corrente in formato unix, lo converte in formato
+    * anno-mese-giorno e restituisce una stringa
+    * 
+    * @param obj
+    * @return String 
+    * 
+    * @throws JSONException
+    * 
+    */
    public String today (JSONObject obj) throws JSONException {
 	   long unix_seconds = obj.getLong("dt");
        Date date = new Date(unix_seconds*1000L); 
@@ -56,6 +84,15 @@ public class Util {
        return java_date;
    }
    
+   /**
+    * metodo che prende in ingresso un Vector e una stringa contenente il nome della città e restituisce un oggetto Media_Varianza
+    * contenente le statistiche 
+    * 
+    * @param v
+    * @param nome
+    * 
+    * @return Media_Varianza
+    */
    public Media_Varianza calcolastats(Vector<JSONObject> v, String nome) {
 	   double mediatemp,mediatemp_min,mediatemp_max,mediafeels_like,vartemp;
 	   double sommatemp=0;
@@ -77,40 +114,8 @@ public class Util {
        mediatemp_min=sommamin/v.size();
        mediafeels_like=sommafeels/v.size();
        vartemp= Math.abs(mediafeels_like-mediatemp);
-       Media_Varianza mv= new Media_Varianza (nome,mediatemp_min,mediatemp_max,mediatemp,mediafeels_like,vartemp);
+       Media_Varianza mv= new Media_Varianza (nome,mediatemp,mediatemp_max,mediatemp_min,mediafeels_like,vartemp);
        return mv;
    }
 
-public CurrentService getS() {
-	return s;
-}
-
-public void setS(CurrentService s) {
-	this.s = s;
-}
-
-public Vector<JSONObject> getObj() {
-	return obj;
-}
-
-public void setObj(Vector<JSONObject> obj) {
-	this.obj = obj;
-}
-
-public int getCounter() {
-	return counter;
-}
-
-public void setCounter(int counter) {
-	this.counter = counter;
-}
-
-public Vector<Temperatura> getV() {
-	return v;
-}
-
-public void setV(Vector<Temperatura> v) {
-	this.v = v;
-}
-    
 }
