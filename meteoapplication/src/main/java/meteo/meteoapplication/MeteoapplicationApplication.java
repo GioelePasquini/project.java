94402e68241a42026a00db515b4f3f97e5f4f32a
umbertomaraglino <74837250+umbertomaraglino@users.noreply.github.com>
2021-01-20 16:24:25 +0100

    aggiunto javadoc

--- meteoapplication/src/main/java/meteo/meteoapplication/MeteoapplicationApplication.java
+++ meteoapplication/src/main/java/meteo/meteoapplication/MeteoapplicationApplication.java
@@ -1,15 +1,30 @@
 package meteo.meteoapplication;
 
+ 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
  
+/**
+ * 
+ * @author Gioele Pasquini
+ * @author Umberto Maraglino
+ *
+ */
+
+ 
+
 @SpringBootApplication
 public class MeteoapplicationApplication {
 
  
 
+ 
+
+ 
+
     public static void main(String[] args) {
-    	SpringApplication.run(MeteoapplicationApplication.class, args);
-    	
-    }		
+        SpringApplication.run(MeteoapplicationApplication.class, args);
+        
+    }        
 }
