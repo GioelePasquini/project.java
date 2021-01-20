7156a306abd9c834ae2bd71c201e0f4e3403db54
GioelePasquini <74837241+GioelePasquini@users.noreply.github.com>
2021-01-20 16:34:13 +0100

    aggiunta javadoc, fill_vector e Filtred_by_range

--- meteoapplication/src/main/java/meteo/meteoapplication/filter/Filtraggio.java
+++ meteoapplication/src/main/java/meteo/meteoapplication/filter/Filtraggio.java
@@ -13,8 +13,20 @@ import meteo.meteoapplication.stats.Media_Varianza;
 import meteo.meteoapplication.stats.Varianza;
 import meteo.meteoapplication.util.Util;
 
+/**
+ * 
+ * @author Gioele Pasquini
+ * @author Umberto Maraglino
+ * 
+ * Classe che contiene i metodi utilizzati per i filtri
+ *
+ */
+
 public class Filtraggio {
 	
+/**
+ * Variabili utilizzate in più metodi del controller
+ */
 	
 	Media_Varianza mv=new Media_Varianza();
 	Object ogg=new Object();
@@ -25,6 +37,18 @@ public class Filtraggio {
 	Vector <String> v=new Vector <String>();
 	 
 	 
+	/**
+	 * Metodo che filtra in base al tipo di statistica e alla città
+	 * 
+	 * @param stat tipo di statistica (media, varianza o entrambe)
+	 * @param citta
+	 * @param obj Vector contenente tutte le info raccolte nello storico
+	 * 
+	 * @return Object contenente la statistica filtrata
+	 * 
+	 * @throws TypeException
+	 * 
+	 */
 	public Object Filtered_by_stat (String stat, String citta, Vector<JSONObject> obj) throws TypeException {
 		
 		
@@ -53,7 +77,21 @@ public class Filtraggio {
 		return ogg;
 	}
 	
-	
+	/**
+	 * Metodo che filtra in base al tipo di relazione (maggiore o minore)
+	 * 
+	 * @param splitted vettore contenente le città inserite dall'utente
+	 * @param param contiene i filtri
+	 * @param type_rel (maggiore o minore)
+	 * @param limite (limite numerico)
+	 * @param type_temp tipo di temperatura da visualizzare 
+	 * 
+	 * @return Vector contenente le informazioni filtrate 
+	 * 
+	 * @throws TypeException
+	 * @throws DatasetException
+	 * 
+	 */
 	
 	public Vector<String> Filtered_by_rel (String [] splitted, Filtro param, String type_rel, double limite,String type_temp) throws TypeException, DatasetException {
 		
@@ -91,6 +129,21 @@ public class Filtraggio {
 	        return v;
 	}
 	
+	/**
+	 * Metodo che filtra in base ai limiti superiore e inferiore passati dall'utente
+	 * 
+	 * @param splitted array che contiene le città
+	 * @param limiteinf 
+	 * @param limitesup
+	 * @param param contiene i filtri
+	 * @param type_temp temperatura da visualizzare
+	 * 
+	 * @return Vector di stringhe contenenti i giorni e le città filtrate
+	 * 
+	 * @throws DatasetExceptionù
+	 * 
+	 */
+	
 	public Vector<String> Filtred_by_limits (String[]splitted, double limiteinf, double limitesup, Filtro param, String type_temp) throws DatasetException {
 		
 		
@@ -111,6 +164,77 @@ public class Filtraggio {
 		}
 		
 		return v;
+	}
+	
+	/**
+	 * Metodo che riempie un Vector con Object contenenti le statistiche filtrate (in base alla città e al tipo di statistica)
+	 * 
+	 * @param dati Vector contenente le info dallo storico
+	 * @param n giorni periodicità
+	 * @param param contiene i filtri
+	 * @param citta 
+	 * 
+	 * @return Vector di object contenente le statistiche filtrate
+	 * 
+	 * @throws TypeException
+	 * 
+	 */
+	
+	 public Vector<Object> fill_vector (Vector<JSONObject> dati,int n, Filtro param, String citta) throws TypeException {
+		 
+		   Vector<Object> risultato=new Vector<Object>();
+		   int k=n;
+		   int n_iter=dati.size()/n;
+		   int counter=1;
+		   for (int i=0; i<dati.size(); i+=n) {
+			   for (int j=i; j<k && k<=dati.size(); j++) {
+				   obj.add(dati.get(j));
+			   }
+			   k+=n;
+			   ogg=Filtered_by_stat(param.getType_stat(),citta, obj);
+			   if (counter<=n_iter) risultato.add(ogg);
+			   obj.clear();
+			   counter++;
+		   }
+		   return risultato;
+	   }
+	 
+	 /**
+	  * Metodo che utilizza fill_vector assegnando la periodicità in funzione della scelta dell'utente
+	  * 
+	  * @param citta
+	  * @param param contiene i filtri
+	  * @param ingresso Vector filtrato da fill_vector
+	  * 
+	  * @return Vector di Object contenente le informazioni filtrate in base alla periodicità
+	  * 
+	  * @throws TypeException
+	  * @throws DatasetException
+	  * 
+	  */
+	
+	public Vector<Object> Filtred_by_range (String citta,Filtro param, Vector <JSONObject> ingresso ) throws TypeException, DatasetException {
+		Vector<Object> res=new Vector<Object>();
+		int n;
+		
+		switch (param.getRange()) {
+		case "Daily" : {
+			n=1;
+			res=fill_vector(ingresso, n, param, citta);break;
+			}		
+		case "Weekly": {
+			n=7;
+			res=fill_vector(ingresso, n, param, citta);break;
+		}
+			
+		case "Customizable" : {
+			n=param.getNum_gg();
+			res=fill_vector(ingresso, n, param, citta);break;
+		}
+			
+		default : throw new DatasetException ("Request not valid.");
+		}
+		return res;
 	}
 
 }
