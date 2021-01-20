3101c9a8a17a6d0dd519a64c57a50124f5f4ac4d
GioelePasquini <74837241+GioelePasquini@users.noreply.github.com>
2021-01-20 16:44:35 +0100

    aggiunta javadoc

--- meteoapplication/src/main/java/meteo/meteoapplication/stats/Statistiche.java
+++ meteoapplication/src/main/java/meteo/meteoapplication/stats/Statistiche.java
@@ -1,8 +1,21 @@
 package meteo.meteoapplication.stats;
 
+/**
+ * 
+ * @author Gioele Pasquini
+ * @author Umberto Maraglino
+ * 
+ * Interfaccia Statistica
+ *
+ */
 
 public interface Statistiche {
 	
+	/**
+	 * Metodo che prende in ingreso una Media_varianza e restituisce un Object con la statistica
+	 * @param st Media_Varianza
+	 * @return Object 
+	 */
 	public Object stampastats(Media_Varianza st);
 	
 }
